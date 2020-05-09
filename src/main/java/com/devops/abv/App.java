9fa1d6783399e7beb3777927acfd47bb1510d68c
aayushya <aayushya1@gmail.com>
2020-05-09 21:32:24 +0530

    Addition method added

--- src/main/java/com/devops/abv/App.java
+++ src/main/java/com/devops/abv/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        Integer a = 50;
+        Integer b = 40;
+        System.out.println(addition(a,b));
     }
+
+    public static Integer addition(Integer a,Integer b){ return a+b;}
 }
